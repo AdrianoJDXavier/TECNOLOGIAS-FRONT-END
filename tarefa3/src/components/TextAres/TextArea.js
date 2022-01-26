687909c3c95d741adb78a4c5fa375b07d7107c14
Adriano Xavier <aj-dx@hotmail.com>
2022-01-26 14:14:49 -0300

    Adicionando contador de caracteres

---
+++ tarefa3/src/components/TextAres/TextArea.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import './TextArea.css';
+
+const TextArea = props => {
+
+    const [TextAreaState, setTextAreaState] = useState({
+        qtd_caracter: 0,
+        value: '',
+    });
+
+    function onChangeText(event) {
+        setTextAreaState({ qtd_caracter: event.target.value.length });
+    }
+
+    return (
+        <div>
+            <textarea value={TextAreaState.value} onChange={onChangeText} placeholder='Digite seu texto aqui!'></textarea>
+            <p>Esse texto contém <b>{TextAreaState.qtd_caracter}</b> caracteres</p>
+        </div>
+    );
+
+
+}
+export default TextArea;
