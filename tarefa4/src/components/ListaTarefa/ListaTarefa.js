450e8b02073ef6f7644af4ff221600b64e00d1c3
Adriano Xavier <aj-dx@hotmail.com>
2022-02-03 17:02:45 -0300

    Definindo condicional para palavras ler e escrever

--- tarefa4/src/components/ListaTarefa/ListaTarefa.js
+++ tarefa4/src/components/ListaTarefa/ListaTarefa.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import './ListaTarefa.css'
 
-const ListaTarefa = ({ onChange, onDelete, onFocus, value }) => {
-  return (
-    <div className="container-tarefa">
-      <input type='checkbox' className="check"/>
-      <div className="list-tarefa">
-        <input
-          className="inputList"
-          value={value}
-          onChange={onChange}
-          onfocus={onFocus}
-        />
-      </div>
-      <button className='btn-delete' title='Deletar' onClick={onDelete}>X</button>
-    </div>
-  );
+const ListaTarefa = ({ onChange, onDelete, value }) => {
+    
+       var classe = 'list';
+    
+    
+        if (value.includes('ler') || value.includes('escrever')) {
+            
+                classe = 'inputMark';
+            
+        }
+    
+    const classes = `inputList ${classe}`
+
+    return (
+        <div className="container-tarefa">
+            <input type='checkbox' className="check" />
+            <div className="list-tarefa">
+                <input
+                    className={classes}
+                    value={value}
+                    onChange={onChange}
+                />
+            </div>
+            <button className='btn-delete' title='Deletar' onClick={onDelete}>X</button>
+        </div>
+    );
 };
 
 export default ListaTarefa;
