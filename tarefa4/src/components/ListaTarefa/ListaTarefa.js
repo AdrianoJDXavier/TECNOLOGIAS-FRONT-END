417e32b628dc890b00b10a2b241fc70c475130a6
Adriano Xavier <aj-dx@hotmail.com>
2022-02-03 15:43:23 -0300

    Definindo páginas de estilo

--- tarefa4/src/components/ListaTarefa/ListaTarefa.js
+++ tarefa4/src/components/ListaTarefa/ListaTarefa.js
@@ -3,6 +3,7 @@ import React from 'react';
 const ListaTarefa = ({ onChange, onDelete, onFocus, value }) => {
   return (
     <div className="container-tarefa">
+      <input type='checkbox' className="check"/>
       <div className="list-tarefa">
         <input
           className="inputList"
