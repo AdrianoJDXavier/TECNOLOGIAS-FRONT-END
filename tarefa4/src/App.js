f54d8cd339c2a3a6c336a2f7646adaaece67f18a
Adriano Xavier <aj-dx@hotmail.com>
2022-02-03 15:35:34 -0300

    Criando componente para inserção de tarefas

--- tarefa4/src/App.js
+++ tarefa4/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import NovaTarefa from './components/NovaTarefa/NovaTarefa';
+import { useState } from 'react';
 
-function App() {
+
+const App = () => {
+const [tarefas, setTarefas] = useState([]);
+
+const addNovaTarefa = (task) => {
+  const copiaTarefa = Array.from(tarefas);
+  copiaTarefa.push({ id: tarefas.length, value: task });
+  setTarefas(copiaTarefa);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NovaTarefa onSubmit={addNovaTarefa} />
     </div>
   );
 }
---
+++ tarefa4/src/components/NovaTarefa/NovaTarefa.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+const NovaTarefa = ({ onSubmit }) => {
+
+  const [newTarefa, setNewTarefa] = useState({
+    tarefa: '',
+  });
+
+  const NovaTarefa = ({ target }) => {
+    setNewTarefa(target.value);
+  }
+
+  const submit = (e) => {
+    e.preventDefault();
+    onSubmit(newTarefa);
+    limpaCampo();
+  }
+
+  const limpaCampo = () => {
+    setNewTarefa({
+      tarefa: '',
+    })
+  }
+
+  return (
+    <div className='div_form'>
+      <form onSubmit={submit}>
+        <div className="input-container">
+          <input id="tarefa" className="input" value={newTarefa.tarefa} type="text" pattern=".+" required onChange={NovaTarefa} />
+          <label className="label" htmlFor="tarefa">Digite a tarefa</label>
+        </div>
+        <button className='btn-submit' type="submit">
+          +
+        </button>
+      </form>
+    </div>
+  )
+};
+
+export default NovaTarefa;
