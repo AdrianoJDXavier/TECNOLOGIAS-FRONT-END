52ab63c35e7e2197766d3c9bc6686f087a3675b0
Adriano Xavier <aj-dx@hotmail.com>
2022-02-10 12:37:41 -0300

    Criando component SignForm

---
+++ tarefa5/src/components/SignForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SignForm = () => {
+
+    return (
+        <div>
+            <h3>Sign up to my newslatter</h3>
+            <form>
+                <input />
+                <button type="submit">
+                    Sign up
+                </button>
+            </form>
+        </div>
+    )
+};
+
+export default SignForm;
